e45b7190ba02a6331740f98e747b97c148fb3731
Joris <Joris.grau2@gmail.com>
2023-03-08 01:04:13 +0100

    Started working on the runners

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -12,6 +12,14 @@ public class GameManager : MonoBehaviour
     Grid<int> testGrid;
     [SerializeField] GameObject gridObject;
 
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +33,17 @@ public class GameManager : MonoBehaviour
         Instantiate(gridObject, new Vector3(x, 0.01f, y) * cellSize, Quaternion.identity,transform);
         return 0;
     }
+
+    public Vector3 GetRandomTargetPos()
+    {
+        var xRand = Random.Range(0, fieldSizeX);
+        var yRand = Random.Range(0, fieldSizeY);
+        var target = new Vector3(xRand, 0, yRand);
+
+        return target;
+    }
+
+
 
     // Update is called once per frame
     void Update()
--- Assets/Runner.cs
+++ Assets/Runner.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Runner : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
+    int currentPathIndex = 0;
+    List<Vector3> pathVectorList;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,50 @@ public class Runner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (pathVectorList != null && pathVectorList.Count != 0)
+        {
+            Debug.Log(currentPathIndex + "  " + pathVectorList.Count);
+            Vector3 targetPosition = pathVectorList[currentPathIndex];
+            if(Vector3.Distance(transform.position, targetPosition) > 1f)
+            {
+                Vector3 moveDir = (targetPosition - transform.position).normalized;
+
+                float distanceBefore = Vector3.Distance(transform.position, targetPosition);
+                //enable animation
+                transform.position = transform.position + moveDir * moveSpeed* Time.deltaTime;
+            }
+            else
+            {
+                currentPathIndex++;
+                if(currentPathIndex >= pathVectorList.Count)
+                {
+                    pathVectorList = null;
+                    //Disable animation
+                    SetTargetPosition(GameManager.instance.GetRandomTargetPos());
+                }
+            }
+        }
+        else
+        {
+            //Disable animation
+            SetTargetPosition(GameManager.instance.GetRandomTargetPos());
+
+        }
+    }
+
+    public void SetTargetPosition(Vector3 targetPosition)
+    {
+        currentPathIndex = 0;
+        pathVectorList = Pathfinding.instance.FindPath(transform.position, targetPosition);
+
+        if(pathVectorList != null && pathVectorList.Count > 1)
+        {
+            pathVectorList.RemoveAt(0);
+        }
     }
 }
